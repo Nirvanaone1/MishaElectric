995020fcf54ec4f5e32d6693591296f285a29e55
Nirvanaone1 <fkingbastard@mail.ru>
2023-09-17 14:59:25 +0300

    Initial commit

---
+++ Misha/src/main/java/sia/Misha/MishaApplication.java
@@ -0,0 +1,13 @@
+package sia.Misha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MishaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MishaApplication.class, args);
+	}
+
+}
